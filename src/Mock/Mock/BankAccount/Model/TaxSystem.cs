2044ae8946697a3411b57f90932ddd5bbedf7d79
Alex Oancea <alx.oancea@gmail.com>
2017-02-12 00:15:51 +0200

    feat(bank-account): tax system models

---
+++ src/Mock/Mock/BankAccount/Model/TaxSystem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Ragnar.Mock.BankAccount.Model
+{
+    public class TaxSystem
+    {
+        public DateTime StartDate { get; set; }
+
+        public Policy[] Policies { get; set; }
+    }
+
+    public struct Policy
+    {
+        public PolicyType Type { get; set; }
+
+        public ComparisonAction Action { get; set; }
+
+        public object ComparisonValue { get; set; }
+
+        public decimal TaxValue { get; set; }
+    }
+
+    public enum PolicyType
+    {
+        Amount = 1,
+        Age = 2
+    }
+
+    public enum ComparisonAction
+    {
+        LessThan,
+        LessOrEqualThan,
+        Equal,
+        GreaterThan,
+        GreaterOrEqualThan
+    }
+}
