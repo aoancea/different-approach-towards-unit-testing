64520270d1cee539c669e1b7079861081705d00c
Alex Oancea <alx.oancea@gmail.com>
2017-03-08 23:41:20 +0200

    feat(interest-mock-driven): adding InterestCalculatorTest

---
+++ src/MockDriven/UnitTesting/Interest/Calculator/InterestCalculatorTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ragnar.MockDriven.UnitTesting.Interest.Calculator
+{
+    [TestClass]
+    public class InterestCalculatorTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+
+        }
+    }
+}
