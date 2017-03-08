f712cb604576bb077c55b6ac624b04c899a94987
Alex Oancea <alx.oancea@gmail.com>
2017-03-08 23:12:25 +0200

    feat(interest-integration-driven): adding projects

---
+++ src/IntegrationDriven/UnitTesting/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ragnar.IntegrationDriven.UnitTesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
